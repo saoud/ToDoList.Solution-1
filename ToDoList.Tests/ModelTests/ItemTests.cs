7bde752c5626e47c1d06e21fb6f9cfd5caf48057
HR Williams <halrubinwilliams@gmail.com>
2021-04-19 20:11:36 -0700

    Add boilerplate

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.TestTools
+{
+  [TestClass]
+  public class ItemTests
+  {
+
+    // Test methods will go here.
+
+  }
+}
