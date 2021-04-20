bd8775c5f2abb71ffa967182586a471bcf2b2f3e
HR Williams <halrubinwilliams@gmail.com>
2021-04-19 20:47:51 -0700

    Create Description property and pass test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,9 +10,22 @@ namespace ToDoList.TestTools
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("tests");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      
+      //Act
+      string result = newItem.Description;
+      
+      //Assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
