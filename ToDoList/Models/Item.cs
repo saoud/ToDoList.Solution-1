8605dc55ff405722fdabd3c4da2ffe5f3b142260
HR Williams <halrubinwilliams@gmail.com>
2021-04-25 16:11:25 -0700

    Update file structure, add MVC framework package

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
-namespace ToDoList.Models
 
+namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set;}
-    private static List<Item> _instances = new List<Item> {};
+    private static List<Item> _instances = new List<Item> { };
+
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
     public static List<Item> GetAll()
     {
       return _instances;
@@ -15,12 +21,5 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
-
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-    }
-
   }
 }
