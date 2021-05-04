86bfa69d5e3b824a317e222df8c56c82f63255c0
HR Williams <halrubinwilliams@gmail.com>
2021-05-03 20:31:20 -0700

    Add good fail test for returning correct Item

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -91,6 +91,19 @@ namespace ToDoList.Tests
 
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      Item result = new Item("Incorrect test item");
+
+      Assert.AreEqual(newItem2, result);
+    }
   }
 }
 
