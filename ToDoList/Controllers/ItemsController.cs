0b0f2b5b965d344e960244599c2ef3aa45609d65
HR Williams <halrubinwilliams@gmail.com>
2021-05-04 14:13:43 -0700

    Add show route to ItemsController

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -34,5 +34,12 @@ namespace ToDoList.Controllers
       Item.ClearAll();
       return View();
     }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }
   }
 }
